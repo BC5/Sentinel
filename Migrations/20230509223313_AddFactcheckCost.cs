d7c3c8b3c1dd7085d4e0f473fbf8c604cf1ea42b
BC5 <6416350+BC5@users.noreply.github.com>
2023-05-13 23:43:05 +0100

    Db migrations

---
+++ Migrations/20230509223313_AddFactcheckCost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sentinel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFactcheckCost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FactcheckCost",
+                table: "Servers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FactcheckCost",
+                table: "Servers");
+        }
+    }
+}
